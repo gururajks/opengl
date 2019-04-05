0b78e8c688e2d6b771035de2946defe1e43d2f5a
Gururaj Sridhar <gururaj.sridhar@autodesk.com>
2019-04-05 00:37:38 -0400

    textures added in project

--- OpenGl/OpenGl/fragshader.glsl
+++ OpenGl/OpenGl/fragshader.glsl
@@ -2,8 +2,12 @@
 
 out vec4 FragColor;
 in vec3 outColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+uniform sampler2D ourTexture2;
 
 void main()
 {
-   FragColor = vec4(outColor, 1.0f);
+   FragColor = mix(texture(ourTexture, TexCoord), texture(ourTexture2, TexCoord), 1); 
 }
--- OpenGl/OpenGl/main.cpp
+++ OpenGl/OpenGl/main.cpp
@@ -7,6 +7,9 @@
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
 
+#include<filesystem>
+#include "stb_image.h"
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -44,29 +47,25 @@ int main()
     }
 
     float vertices[] = {
-        -0.4f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.6f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-        -0.2f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+		 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f
     };
 
-    float secondVertices[] = {
-        0.9f, 0.5f, 0.0f,
-        0.5f, 0.0, 0.0f,
-        0.9f, -0.5f, 0.0f
-    };    
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
 
 	ShaderReader vSRead;
-	vSRead.readFile("E:\\opengl\\OpenGl\\OpenGl\\vertexshader.glsl");
+	vSRead.readFile("vertexshader.glsl");
 	const char* vertexShaderSource = vSRead.getShaderSource();
 
 	ShaderReader fSRead;
-	fSRead.readFile("E:\\opengl\\OpenGl\\OpenGl\\fragshader.glsl");
+	fSRead.readFile("fragshader.glsl");
 	const char* fragmentShaderSource = fSRead.getShaderSource();
-
-	ShaderReader fSRead2;
-	fSRead2.readFile("E:\\opengl\\OpenGl\\OpenGl\\secondfragshader.glsl");
-	const char* secondFragmentShaderSource = fSRead2.getShaderSource();
-	    
+		
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -93,16 +92,6 @@ int main()
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    unsigned int secondFragmentShader;
-    secondFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(secondFragmentShader, 1, &secondFragmentShaderSource, NULL);
-    glCompileShader(secondFragmentShader);
-    glGetShaderiv(secondFragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(secondFragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
 
     //link the shader program
     unsigned int shaderProgram;
@@ -117,54 +106,85 @@ int main()
         std::cout << "ERROR::SHADER::LINK::COMPILATION_FAILED\n" << infoLog << std::endl;
     }    
     glDeleteShader(fragmentShader);
-
-    //link the shader program
-    unsigned int secondShaderProgram;
-    secondShaderProgram = glCreateProgram();
-    glAttachShader(secondShaderProgram, vertexShader);
-    glAttachShader(secondShaderProgram, secondFragmentShader);
-    glLinkProgram(secondShaderProgram);
-    glGetShaderiv(secondShaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(secondShaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::LINK::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
     glDeleteShader(vertexShader);
-    glDeleteShader(secondFragmentShader);
-
-
 
     unsigned int VAO[2];
     unsigned int VBO[2];
+	unsigned int EBO[2];
     glGenVertexArrays(2, VAO);
     glGenBuffers(2, VBO);
-
+	glGenBuffers(2, EBO);
     glBindVertexArray(VAO[0]);
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);    
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3* sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3* sizeof(float)));
     glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+    
+	//textures
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+	unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
-    glBindVertexArray(VAO[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(secondVertices), secondVertices, GL_STATIC_DRAW);    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+	//textures2
+	//unsigned int texture2;
+	//glGenTextures(1, &texture2);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//// load image, create texture and generate mipmaps
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+	//stbi_set_flip_vertically_on_load(true);
+	//// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+	//data = stbi_load("smiley.jpg", &width, &height, &nrChannels, 0);
+	//if (data)
+	//{		
+	//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	//	glGenerateMipmap(GL_TEXTURE_2D);
+	//}
+	//else
+	//{
+	//	std::cout << "Failed to load texture" << std::endl;
+	//}
+	//stbi_image_free(data);
+	
 
 	glm::mat4 transformMat = glm::mat4(	1.0f, 0.0f, 0.0f, 0.0f,
 										0.0f, 1.0f, 0.0f, 0.0f, 
 										0.0f, 0.0f, 1.0f, 0.0f, 
 										0.0f, 0.0f, 0.0f, 1.0f) ;
 	
-	transformMat = glm::scale(transformMat, glm::vec3(0.5f, 1.0f, 0.0f));
+	transformMat = glm::scale(transformMat, glm::vec3(1.0f, 1.5f, 1.0f));
+	glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+	//glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
 	
-		
+	glm::mat4 model = glm::mat4(1);
+	glm::mat4 view = glm::mat4(1);
+	glm::mat4 proj = glm::mat4(1);
+
 
     // render loop
     // swap buffers to render images
@@ -177,23 +197,17 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 		float currentTime = glfwGetTime();
 
-        glUseProgram(shaderProgram);
-		unsigned int f1transformLoc = glGetUniformLocation(secondShaderProgram, "transform");
-		//transformMat = glm::rotate(transformMat, glm::radians(currentTime), glm::vec3(0.0f, 0.0f, 1.0f));
-		//transformMat = glm::scale(transformMat, glm::vec3(sin(currentTime) * 10.0f, sin(currentTime) * 10.0f, 0.0f));
+        
+		unsigned int f1transformLoc = glGetUniformLocation(shaderProgram, "transform");
 		glUniformMatrix4fv(f1transformLoc, 1, GL_FALSE, glm::value_ptr(transformMat));
-
+		
+		/*glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture2);*/
         glBindVertexArray(VAO[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-		glUseProgram(secondShaderProgram);		
-
-		unsigned int transformLoc = glGetUniformLocation(secondShaderProgram, "transform");
-		//transformMat = glm::rotate(transformMat, glm::radians(currentTime), glm::vec3(0.0f, 0.0f, 1.0f));
-		//transformMat = glm::scale(transformMat, glm::vec3(sin(currentTime) * 10.0f, sin(currentTime) * 10.0f, 0.0f));
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transformMat));
-
-		glBindVertexArray(VAO[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glUseProgram(shaderProgram);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -201,7 +215,7 @@ int main()
 
     glDeleteVertexArrays(2, VAO);
     glDeleteBuffers(2, VBO);
-
+	glDeleteBuffers(2, EBO);
     glfwTerminate();
     return 0;
 }
